9e29b5d29cc692fb79138d6d6a5d94a6b1e114cf
Scott Hurlow <Scottyhurlow@gmail.com>
2019-03-25 00:38:21 -0700

    renames dev server file, cleanup deps

---
+++ server.js
@@ -0,0 +1,26 @@
+const micro = require('micro')
+const handler = require('serve-handler');
+require('dotenv').config()
+// require('dotenv').config({ path: './.env.prod' }) // test with prod env
+
+const index = require('./api/index')
+const token = require('./api/token')
+const scrape = require('./api/scrape')
+const login = require('./api/login')
+
+const routes = {
+  '/api': index,
+  '/api/token': token,
+  '/api/scrape': scrape,
+  '/api/login': login,
+}
+
+const server = micro(async (req, res) => {
+  const route = routes[req.url]
+  if(route) return await route(req, res)
+  return handler(req, res, {
+    public: 'src'
+  })
+})
+
+server.listen(3000)
