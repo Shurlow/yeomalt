990c62d86719bc4a07e4df6e91267c67e3687945
Scott Hurlow <Scottyhurlow@gmail.com>
2019-09-08 01:24:44 -0700

    adds instagram gatsby source

--- client/src/components/postCard.js
+++ client/src/components/postCard.js
@@ -1,23 +1,14 @@
 import React from "react"
-import { Link } from "gatsby"
 
-export default props => (
-  <article
-    className={`post-card ${props.count % 3 === 0 && `post-card-large`} ${
-      props.postClass
-    } ${props.node.frontmatter.thumbnail ? `with-image` : `no-image`}`}
-    style={
-      props.node.frontmatter.thumbnail && {
-        backgroundImage: `url(${props.node.frontmatter.thumbnail.childImageSharp.fluid.src})`,
-      }
-    }
-  >
-    <Link to={props.node.fields.slug} className="post-card-link">
+export default ({ img, count, caption }) => {
+  return (
+    <article
+      className={`post post-card ${count % 3 === 0 && `post-card-large`}`}
+      style={{ backgroundImage: `url(${img})` }}
+    >
       <div className="post-card-content">
-        <h2 className="post-card-title">
-          {props.node.frontmatter.title || props.node.fields.slug}
-        </h2>
+        <h2 className="post-card-title">{caption || ""}</h2>
       </div>
-    </Link>
-  </article>
-)
+    </article>
+  )
+}
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql, StaticQuery } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-// import PostCard from "../components/postCard"
+import PostCard from "../components/postCard"
 
 // import "../utils/global.scss"
 import "../utils/normalize.css"
@@ -11,8 +11,11 @@ import "../utils/css/screen.css"
 //TODO: switch to staticQuery, get rid of comments, remove unnecessary components, export as draft template
 const BlogIndex = ({ data }, location) => {
   const siteTitle = data.site.siteMetadata.title
-  // const posts = data.allMarkdownRemark.edges
-  // let postCounter = 0
+  const posts = data.allInstaNode.edges
+  console.log(posts)
+  console.log(
+    posts[0].node.caption.replace(/\B(\#[a-zA-Z0-9]+\b|\.)(?!;)/g, " ")
+  )
 
   return (
     <Layout title={siteTitle}>
@@ -25,7 +28,20 @@ const BlogIndex = ({ data }, location) => {
           </h2>
         </header>
       )}
-      <div className="post-feed">no posts</div>
+      <div className="post-feed">
+        {posts.map((post, i) => {
+          return (
+            <PostCard
+              img={post.node.localFile.childImageSharp.fixed.src}
+              caption={post.node.caption.replace(
+                /\B(\#[a-zA-Z0-9]+\b|\.)(?!;)/g,
+                ""
+              )}
+              count={i}
+            />
+          )
+        })}
+      </div>
     </Layout>
   )
 }
@@ -36,6 +52,37 @@ const indexQuery = graphql`
       siteMetadata {
         title
         description
+      }
+    }
+    allInstaNode {
+      edges {
+        node {
+          id
+          mediaType
+          preview
+          original
+          caption
+          localFile {
+            childImageSharp {
+              fixed(width: 400) {
+                base64
+                width
+                height
+                src
+                srcSet
+              }
+            }
+          }
+          thumbnails {
+            src
+            config_width
+            config_height
+          }
+          dimensions {
+            height
+            width
+          }
+        }
       }
     }
   }
